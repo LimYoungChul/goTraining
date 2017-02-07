e6127f52c497bed3bdb9c4266336c750790fe521
Henning Vestergaard <Henning Vestergaard>
2017-02-07 02:38:04 +0100

    split up combination array

--- crackFreeWalls2/wall.go
+++ crackFreeWalls2/wall.go
@@ -3,30 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(wall(32, 10))
+	fmt.Println(wall(20, 10))
 }
 
-func wall(width, height int) int {
+func wall(width, height int) int64 {
 
-	total := 0
+	var total int64
 
 	combinations := findCombinations(width)
 
-	fmt.Println(len(combinations))
+	combiPointer := &combinations
 
-	for _, a := range combinations {
-
-		buildWall(a, combinations, 0, height, &total)
-
+	for i := 0; i < 4; i++ {
+		for _, a := range combinations[i] {
+			if i%2 == 0 {
+				buildWall(a, combiPointer[i+1], 0, height, i+1, &total, combiPointer)
+			} else {
+				buildWall(a, combiPointer[i-1], 0, height, i-1, &total, combiPointer)
+			}
+		}
 	}
-
 	return total
 }
 
-func buildWall(a []int, combi [][]int, level, height int, total *int) {
+func buildWall(a []int, combi [][]int, level, height, index int, total *int64, pointer *[4][][]int) {
 	level++
 	var canCombine bool
-
 	for _, a1 := range combi {
 		canCombine = true
 		for _, value := range a {
@@ -38,11 +40,15 @@ func buildWall(a []int, combi [][]int, level, height int, total *int) {
 					canCombine = false
 					break
 				}
-
 			}
 		}
 		if canCombine == true && level < height {
-			buildWall(a1, combi, level, height, total)
+
+			if index%2 == 0 {
+				buildWall(a1, pointer[index+1], level, height, index+1, total, pointer)
+			} else {
+				buildWall(a1, pointer[index-1], level, height, index-1, total, pointer)
+			}
 		} else if level == height {
 			*total++
 			break
@@ -50,12 +56,13 @@ func buildWall(a []int, combi [][]int, level, height int, total *int) {
 	}
 }
 
-func findCombinations(width int) [][]int {
+func findCombinations(width int) [4][][]int {
 	var i int
-	//var c int
 	var tmp int
+	var tmpInt1 int
+	var tmpInt2 int
 	open := make([][]int, 0, 100)
-	solutions := make([][]int, 0, 100)
+	var solutionsHolder [4][][]int
 	open = append(open, []int{3, 2})
 	tmpArray := make([]int, 0, 100)
 
@@ -71,16 +78,28 @@ func findCombinations(width int) [][]int {
 				solutionArray := make([]int, len(tmpArray)-1)
 				counter2 := 0
 				for n := 0; n < len(tmpArray)-1; n++ {
+					if n == 0 {
+						tmpInt1 = tmpArray[n] % 2
+					}
 					counter2 += tmpArray[n]
 					solutionArray[n] = counter2
 				}
-				solutions = append(solutions, solutionArray)
+				tmpInt2 = counter2 % 2
+				if tmpInt1 == 0 && tmpInt2 == 0 {
+					solutionsHolder[0] = append(solutionsHolder[0], solutionArray)
+				} else if tmpInt1 == 1 && tmpInt2 == 1 {
+					solutionsHolder[1] = append(solutionsHolder[1], solutionArray)
+				} else if tmpInt1 == 1 && tmpInt2 == 0 {
+					solutionsHolder[2] = append(solutionsHolder[2], solutionArray)
+				} else {
+					solutionsHolder[3] = append(solutionsHolder[3], solutionArray)
+				}
 
 				for _, v := range open {
 					tmp += len(v)
 				}
 				if tmp == 0 {
-					return solutions
+					return solutionsHolder
 				}
 				tmp = 0
 			} else if counter > width {
@@ -88,7 +107,7 @@ func findCombinations(width int) [][]int {
 					tmp += len(v)
 				}
 				if tmp == 0 {
-					return solutions
+					return solutionsHolder
 				}
 				tmp = 0
 			} else if counter < width {
