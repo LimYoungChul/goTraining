f0ec4608d8d94202349787993c5a76b045911f36
LimYoungChul <henningthehuman@gmail.com>
2017-11-04 13:27:22 +0100

    euler and soko

--- goTraining/projectEuler/Problem_12/main.go
+++ goTraining/projectEuler/Problem_12/main.go
@@ -7,7 +7,35 @@ import (
 
 func main() {
 	start := time.Now()
+	i := 0
+	var x int
+	var n int
 
+	for x < 500 {
+		i++
+		n = i * (i + 1) / 2
+		x = factorCounter(n)
+	}
+
+	fmt.Println(n)
+	fmt.Println(x)
 	elapsed := time.Since(start)
 	fmt.Println("Runtime", elapsed)
+}
+
+func factorCounter(number int) int {
+	counter := 0
+	d := number
+
+	for i := 1; i < number; i++ {
+		if i >= d { //this should ALWAYS break the loop. i guess unless the number is a prime, but we never pass the function any primes i think
+			break
+		}
+		if number%i == 0 {
+			counter += 2 //we add 2, cause everytime we find a number we also find one in the other end. eg: 20 gives 1 and 20, 2 and 10, 4 and 5. "d will be = 5 and "i" will be 5 next time so it breaks the loop
+			d = number / i
+		}
+	}
+
+	return counter
 }
